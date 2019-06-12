af18d2bb677c9c14080660ab6ec53509ccb47caf
Chris Cheong <chrisxcheong@gmail.com>
2019-06-12 21:13:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
 def turn_count(board)
   turn_counter = 0
   board.each do |position|
-    if position != X || position != O
+    if position != " " && position != ""
       turn_counter += 1
     end
     end
+    return turn_counter
   end
